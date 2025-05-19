a7f2d0fdb21b7dd1699e9ddf3595eb29aefc8043
KastanEr <skuller320@naver.com>
2025-05-19 15:39:55 +0900

    hotfix: oauth2 지원 코드 제거

--- src/main/java/shop/ink3/auth/config/SecurityConfig.java
+++ src/main/java/shop/ink3/auth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package shop.ink3.auth.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,15 +7,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import shop.ink3.auth.handler.CustomAuthenticationFailureHandler;
-import shop.ink3.auth.service.CustomOAuth2UserService;
 
-@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -29,11 +22,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .oauth2Login(oauth -> oauth
-                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .failureHandler(customAuthenticationFailureHandler)
-                );
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
